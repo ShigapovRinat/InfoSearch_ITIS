e0fcc63164cd1c9e9c28a5162b2e504c6dfba40c
Rinat_Shigapov <rinat.shigapov@simbirsoft.com>
2022-03-02 10:00:40 +0300

    task 1

--- task1/parser.py
+++ task1/parser.py
@@ -8,15 +8,15 @@ def save(url):
         file = open('data/' + str(i) + ".html", 'a', encoding="utf-8")
         file.write(str(bs))
         file.flush()
-        index.write(str(i-8471213) + ' ' + url + '\n')
+        index.write(str(i) + ' ' + url + '\n')
         index.flush()
     except Exception:
         print('Exception in url: ' + url)
 
 
 if __name__ == '__main__':
-    mainUrl = 'https://www.nhl.com/player/'
-    index = open("data/list.txt", 'a')
+    mainUrl = 'https://www.litmir.me/br/?b=217310&p='
+    index = open("data/index.txt", 'a')
 
-    for i in range(8471214, 8471350):
+    for i in range(1, 110):
         save(mainUrl + str(i))
